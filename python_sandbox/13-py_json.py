9460b7ea5a93fc28e706352e04ceed603bf2ae11
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2026-01-17 03:28:56 +0000

    json

---
+++ python_sandbox/13-py_json.py
@@ -0,0 +1,25 @@
+# JSON is commonly used with data APIS. Here how we can parse JSON into a Python
+# dictionary
+
+import json
+
+# Sample JSON
+userJSON = '{"first_name":"John","last_name":"Doe","age":30}'
+
+print(userJSON)
+
+# Parse to dict
+user = json.loads(userJSON)
+
+print(user)
+print(user["first_name"])
+
+# Dictionary to JSON
+
+car = {"make": "Vauxhall", "model": "Astra", "year": 2010}
+
+print(car)
+
+carJSON = json.dumps(car)
+
+print(carJSON)
